b8c991c2851b100e7a2f2db70650311c09d40d6d
Milad Farazian <mfarazia@ucsc.edu>
2021-03-26 10:50:32 -0700

    Upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Wrapper = styled.main`
+  width: 50%;
+  height: 50%;
+`;
+
+class App extends Component {
+  render(){
+    return (
+
+      <Wrapper>
+        <Map className="popup\_inner" google={this.props.google} zoom={13} 
+          initialCenter={
+                  {
+                      lat: 36.984117,
+                      lng: -122.030792
+                  }
+                  }>
+            <Marker onClick={this.onMarkerClick}
+              name={'University of California, Santa Cruz'}
+              position={{lat: 36.9881, lng: -122.0582}} 
+            >
+            </Marker>
+            <Marker onClick={this.onMarkerClick}
+              name={'Santa Cruz'}
+              position={{lat: 36.974117, lng: -122.030792}} />
+        </Map>
+      </Wrapper>  
+    )
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyBfuC9_pclonmIh4lVWRooHqjsGNOlvziQ",
+})(App)
